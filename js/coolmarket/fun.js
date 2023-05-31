e557f86e069d49835f12cade6e64cd3b0369d3fc
vkaja <tom1870699@gmail.com>
2023-05-31 16:33:04 +0800

    bug fix

--- js/coolmarket/fun.js
+++ js/coolmarket/fun.js
@@ -2,5 +2,6 @@ let body = $response.body;
 let obj = JSON.parse(body);
 let like = 999;
 obj.data[0].notifyCount.feedlike = like;
+console.log("fun.js:feedlike will be: ",like)
 body = JSON.stringify(obj)
 $done(body)
