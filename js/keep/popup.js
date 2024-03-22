4358824251c68695894e3d777409e0f1eb92b784
vkaja <tom1870699@gmail.com>
2024-03-22 13:29:23 +0800

    add keep

---
+++ js/keep/popup.js
@@ -0,0 +1,10 @@
+let obj = JSON.parse($response.body);
+if (obj.data.popUpCode){
+    let popUpCode = obj.data.popUpCode;
+    let errorCode = obj.errorCode = $persistenStore.read(errorCode);
+    let ok = obj.ok = $persistentStore.read(ok);
+    let body = JSON.stringify(obj);
+    $done({body})
+    $notification.post("keep_popup",`popUpCode:${popUpCode}, errorCode:${errorCode}, ok:${ok}`);
+}
+$done({})
